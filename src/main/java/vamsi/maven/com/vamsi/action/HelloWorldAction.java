9d923d2e0cb068c22ea2c9de29646791ff928608
RAGHU NANI <RAGHU NANI@DESKTOP-4PEU4UK>
2023-08-18 17:17:02 +0530

    first commit

--- src/main/java/vamsi/maven/com/vamsi/action/HelloWorldAction.java
+++ src/main/java/vamsi/maven/com/vamsi/action/HelloWorldAction.java
@@ -26,6 +26,6 @@ public class HelloWorldAction implements DefaultAction<BaseUserContext, String> 
 
   @Action
   public void sayHelloToAll() {
-    sampleClientAction.sayHello("Hello everyone!");
+    sampleClientAction.sayHello("Hello everyone");
   }
 }
